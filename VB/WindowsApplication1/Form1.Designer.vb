57c1ea713b1a2dd5fb476c3889bc5d7599811334
DevExpressExampleBot <scmaintenance@devexpress.com>
2026-09-23 12:03:39 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.Designer.vb
+++ VB/WindowsApplication1/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace WindowsApplication1
-    Partial Public Class Form1
+Namespace WindowsApplication1
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,27 +12,27 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim spellCheckerOpenOfficeDictionary1 As New DevExpress.XtraSpellChecker.SpellCheckerOpenOfficeDictionary()
-            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-            Dim spellCheckerCustomDictionary1 As New DevExpress.XtraSpellChecker.SpellCheckerCustomDictionary()
-            Dim optionsSpelling1 As New DevExpress.XtraSpellChecker.OptionsSpelling()
+            Dim spellCheckerOpenOfficeDictionary1 As DevExpress.XtraSpellChecker.SpellCheckerOpenOfficeDictionary = New DevExpress.XtraSpellChecker.SpellCheckerOpenOfficeDictionary()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WindowsApplication1.Form1))
+            Dim spellCheckerCustomDictionary1 As DevExpress.XtraSpellChecker.SpellCheckerCustomDictionary = New DevExpress.XtraSpellChecker.SpellCheckerCustomDictionary()
+            Dim optionsSpelling1 As DevExpress.XtraSpellChecker.OptionsSpelling = New DevExpress.XtraSpellChecker.OptionsSpelling()
             Me.spellChecker1 = New DevExpress.XtraSpellChecker.SpellChecker()
             Me.memoEdit1 = New DevExpress.XtraEditors.MemoEdit()
             Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
-            CType(Me.memoEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.memoEdit1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' spellChecker1
@@ -40,13 +42,13 @@
             spellCheckerOpenOfficeDictionary1.CacheKey = Nothing
             spellCheckerOpenOfficeDictionary1.Culture = New System.Globalization.CultureInfo("de-DE")
             spellCheckerOpenOfficeDictionary1.DictionaryPath = "Dictionaries\de_DE.dic"
-            spellCheckerOpenOfficeDictionary1.Encoding = (CType(resources.GetObject("spellCheckerOpenOfficeDictionary1.Encoding"), System.Text.Encoding))
+            spellCheckerOpenOfficeDictionary1.Encoding = CType((resources.GetObject("spellCheckerOpenOfficeDictionary1.Encoding")), System.Text.Encoding)
             spellCheckerOpenOfficeDictionary1.GrammarPath = "Dictionaries\de_DE.aff"
             spellCheckerCustomDictionary1.AlphabetPath = "Dictionaries\GermanAlphabet.txt"
             spellCheckerCustomDictionary1.CacheKey = Nothing
             spellCheckerCustomDictionary1.Culture = New System.Globalization.CultureInfo("de-DE")
             spellCheckerCustomDictionary1.DictionaryPath = "Dictionaries\CustomGermanDictionary.txt"
-            spellCheckerCustomDictionary1.Encoding = (CType(resources.GetObject("spellCheckerCustomDictionary1.Encoding"), System.Text.Encoding))
+            spellCheckerCustomDictionary1.Encoding = CType((resources.GetObject("spellCheckerCustomDictionary1.Encoding")), System.Text.Encoding)
             Me.spellChecker1.Dictionaries.Add(spellCheckerOpenOfficeDictionary1)
             Me.spellChecker1.Dictionaries.Add(spellCheckerCustomDictionary1)
             Me.spellChecker1.ParentContainer = Nothing
@@ -67,6 +69,7 @@
             Me.simpleButton1.Size = New System.Drawing.Size(91, 23)
             Me.simpleButton1.TabIndex = 1
             Me.simpleButton1.Text = "Check spelling"
+            AddHandler Me.simpleButton1.Click, New System.EventHandler(AddressOf Me.simpleButton1_Click)
             ' 
             ' Form1
             ' 
@@ -78,16 +81,16 @@
             Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.memoEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.memoEdit1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+'#End Region
         Private spellChecker1 As DevExpress.XtraSpellChecker.SpellChecker
+
         Private memoEdit1 As DevExpress.XtraEditors.MemoEdit
-        Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
+
+        Private simpleButton1 As DevExpress.XtraEditors.SimpleButton
     End Class
 End Namespace
-
